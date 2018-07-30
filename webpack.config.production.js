1ad02b6f1a90cba62b50c8b359e8606b39edef5c
Mark Borcherding <markborcherding@gmail.com>
2018-07-29 21:11:06 -0500

    tweak path

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/dist/"
+    publicPath: "./pointless-javascript/dist/"
   },
   plugins: [
     new webpack.DefinePlugin({
